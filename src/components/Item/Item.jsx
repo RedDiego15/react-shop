ae7d222b8a83bf7cc44366e98cf21d2e615f8441
Diego Rojas <Diego-27ro@outlook.com>
2022-04-26 21:59:47 -0500

    change products

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,36 +1,82 @@
-import React from 'react'
-import AddToCarImage from '@icons/bt_add_to_cart.svg'
-import {AppContext} from '@context/AppContext'
+import React from "react";
+import AddToCarImage from "@icons/bt_add_to_cart.svg";
+import { AppContext } from "@context/AppContext";
+import styled from "styled-components";
 
-function Item({item}) {
+const ContainerProductCard = styled.div`
+	width: 240px;
+	@media (max-width: 640px) {
+		width: 160px;
+	}
+`;
+const ProductImage = styled.img`
+	width: 240px;
+	height: 240px;
+	border-radius: 20px;
+	object-fit: cover;
+	@media (max-width: 640px) {
+		width: 160px;
+		height: 160px;
+	}
+`;
+const ContainerProductInfo = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-top: 12px;
+`;
+const StyledFigure = styled.figure`
+	margin: 0;
+`;
+const CartImage = styled.img`
+	width: 35px;
+	height: 35px;
+`;
+const Price = styled.p`
+	font-weight: bold;
+	font-size: ${(props) => props.theme.md};
+	margin-top: 0;
+	margin-bottom: 4px;
+`;
+const ItemTitle = styled.p`
+	font-size: ${(props) => props.theme.sm};
+	margin-top: 0;
+	margin-bottom: 0;
+	color: ${(props) => props.theme.very_light_pink};
+`;
+function Item({ item }) {
+	const [isSelected, setIsSelected] = React.useState(false);
 
-  const [isSelected,setIsSelected] = React.useState(false)
+	const {
+		state: { toggleItemInfo },
+		addToCart,
+		setToggleItemInfo,
+	} = React.useContext(AppContext);
+	const addItem = () => {
+		setIsSelected((prevState) => !prevState);
+		addToCart(item);
+	};
 
-  const {state:{toggleItemInfo},addToCart,setToggleItemInfo} = React.useContext(AppContext)
-  const addItem = () =>{
-    setIsSelected(prevState => !prevState)  
-    addToCart(item);
-    
-  }
-
-  return (
-    <>
-         <div className="product-card">
-            <img 
-            onClick={() => setToggleItemInfo()}
-            src={item.category.image} alt="product-image"/>
-            <div className="product-info">
-            <div>
-                <p>{`$ ${item.price}`}</p>
-                <p>{item.title}</p>
-            </div>
-            <figure onClick={addItem}>
-                <img src={AddToCarImage} alt="product-image"/>
-            </figure>
-            </div>
-        </div>
-    </>
-  )
+	return (
+		<>
+			<ContainerProductCard>
+				<ProductImage
+					onClick={() => setToggleItemInfo()}
+					src={item.category.image}
+					alt="product-image"
+				/>
+				<ContainerProductInfo>
+					<div>
+						<Price>{`$ ${item.price}`}</Price>
+						<ItemTitle>{item.title}</ItemTitle>
+					</div>
+					<StyledFigure onClick={addItem}>
+						<CartImage src={AddToCarImage} alt="product-image" />
+					</StyledFigure>
+				</ContainerProductInfo>
+			</ContainerProductCard>
+		</>
+	);
 }
 
-export {Item}
+export { Item };
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,28 +1,41 @@
-import React from 'react'
-import { ProductsContainer } from '@containers/ProductsContainer'
-import { Item } from '@components/Item/Item'
-import {useGetProducts} from '@hooks/useGetProducts'
+import React from "react";
+import { ProductsContainer } from "@containers/ProductsContainer";
+import { Item } from "@components/Item/Item";
+import { useGetProducts } from "@hooks/useGetProducts";
+import styled from "styled-components";
 
+const MainContainer = styled.section`
+	margin-top: 2rem;
+`;
+const CardsContainer = styled.div`
+	display: grid;
+	grid-template-columns: repeat(auto-fill, 240px);
+	gap: 26px;
+	place-content: center;
+	@media (max-width: 640px) {
+		grid-template-columns: repeat(auto-fill, 160px);
+	}
+`;
 function Products() {
-  const {products, isLoading, errorGetProducts} = useGetProducts(15);
+	const { products, isLoading, errorGetProducts } = useGetProducts(15);
 
-  return (
-    <ProductsContainer>
-      <section className="main-container">
-        <div className="cards-container">
-          {isLoading && <p>Loading...</p>}
-          {errorGetProducts && <p>Something wrong happen try later</p>}
-          {products.map((item) => (
-            <Item
-              ImgUrl={item.category.image}
-              item={item}
-              key={`itemProduct-${item.id}`}
-            />
-          ))}
-        </div>
-      </section>
-    </ProductsContainer>
-  );
+	return (
+		<ProductsContainer>
+			<MainContainer>
+				<CardsContainer>
+					{isLoading && <p>Loading...</p>}
+					{errorGetProducts && <p>Something wrong happen try later</p>}
+					{products.map((item) => (
+						<Item
+							ImgUrl={item.category.image}
+							item={item}
+							key={`itemProduct-${item.id}`}
+						/>
+					))}
+				</CardsContainer>
+			</MainContainer>
+		</ProductsContainer>
+	);
 }
 
-export {Products}
+export { Products };
