ff4dcbf03e5b36ad06de1a184c4c3478362b4080
Diego Rojas <Diego-27ro@outlook.com>
2022-05-14 16:02:31 -0500

    navigate of login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,64 +1,71 @@
-import React from 'react'
-import LogoSale from '@logos/logo_yard_sale.svg';
-  
+import React from "react";
+import LogoSale from "@logos/logo_yard_sale.svg";
+import { useNavigate } from "react-router-dom";
+
 function Login() {
-  const form = React.useRef(null);
+	const form = React.useRef(null);
+	const navigate = useNavigate();
 
-  const handleSubmit = (e)=>{
-    e.preventDefault();
-    const formData = new FormData(form.current);
-    const data = {
-      username:formData.get('email'),
-      password:formData.get('password')
-    }
-    console.log(form.current)
-  }
-    return (
-      <div className="login">
-        <div className="form-container">
-          <img src={LogoSale} alt="logo" className="logo" />
+	const handleLoginSubmit = (e) => {
+		e.preventDefault();
+		const formData = new FormData(form.current);
+		const data = {
+			username: formData.get("email"),
+			password: formData.get("password"),
+		};
+		console.log(form.current);
+		navigate("/");
+	};
+	return (
+		<div className="login">
+			<div className="form-container">
+				<img src={LogoSale} alt="logo" className="logo" />
 
-          <form onSubmit={handleSubmit} action="/" className="form" ref={form}>
-            <label htmlFor="email" className="label">
-              Email address
-            </label>
-            <input
-              type="text"
-              name="email"
-              id="email"
-              placeholder="platzi@example.cm"
-              className="input input-email"
-            />
+				<form
+					onSubmit={handleLoginSubmit}
+					action="/"
+					className="form"
+					ref={form}
+				>
+					<label htmlFor="email" className="label">
+						Email address
+					</label>
+					<input
+						type="text"
+						name="email"
+						id="email"
+						placeholder="platzi@example.cm"
+						className="input input-email"
+					/>
 
-            <label htmlFor="password" className="label">
-              Password
-            </label>
-            <input
-              type="password"
-              name="password"
-              id="password"
-              placeholder="*********"
-              className="input input-password"
-            />
+					<label htmlFor="password" className="label">
+						Password
+					</label>
+					<input
+						type="password"
+						name="password"
+						id="password"
+						placeholder="*********"
+						className="input input-password"
+					/>
 
-            <input
-              //onSubmit={handleSubmit}
-              type="submit"
-              value="Log in"
-              className="primary-button login-button"
-            />
-            <a href="/">Forgot my password</a>
-          </form>
+					<input
+						type="submit"
+						value="Log in"
+						className="primary-button login-button"
+					/>
+					<a href="/">Forgot my password</a>
+				</form>
 
-          <button
-            
-            className="secondary-button signup-button"
-          >
-            Sign up
-          </button>
-        </div>
-      </div>
-    );
+				<button
+					onClick={() => navigate("/create-my-account")}
+					className="secondary-button signup-button"
+				>
+					Sign up
+				</button>
+			</div>
+		</div>
+	);
 }
 
-export {Login}
+export { Login };
--- src/components/MyAccount/EditAccount.jsx
+++ src/components/MyAccount/EditAccount.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import { AccountContainer } from '../../Containers/AccountContainer'
+import React from "react";
+import { AccountContainer } from "@containers/AccountContainer";
 
 function EditAccount() {
-  return (
-    <AccountContainer>
-        <input type="submit" value="Edit" className="secondary-button login-button"/>
-    </AccountContainer>
-  )
+	const handleSubmit = () => {};
+	return (
+		<AccountContainer>
+			<input
+				type="submit"
+				value="Edit"
+				className="secondary-button login-button"
+			/>
+		</AccountContainer>
+	);
 }
 
-export {EditAccount}
+export { EditAccount };
